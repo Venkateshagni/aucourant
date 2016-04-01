bcfcb23853da7ba77a2e60c309f1c9261972ae5c
Venkatesh <venku.agni@gmail.com>
2016-04-01 12:48:06 +0530

    A and B
    A and B is simple c# Program written in console Application.
    This program was asked Microsoft online technical screening. You have to find the bug in the code. I have found the Bug and updated the details in comment lines. The solution works fine.

---
+++ AandB/AandB/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AandB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeTheNumbersMatch(28, 39, 20, 30);
+        }
+        // The following program increases/decreases the value of A and B until A is equal to X and B is equal to Y. Find the bug and its solution:
+        //public static void MakeTheNumbersMatch(int a, int b, int x, int y)
+        //{
+        //    while (a != x && b != y)
+        //    {
+        //        if (a > x)
+        //        {
+        //            a--;
+        //        }
+        //        else
+        //        {
+        //            a++;
+        //        }
+        //        if (b > y)
+        //        {
+        //            b--;
+        //        }
+        //        else
+        //        {
+        //            b++;
+        //        }
+        //    }
+        //}
+         /* Solution */
+        public static void MakeTheNumbersMatch(int a, int b, int x, int y)
+        {
+            while (a != x || b != y) // solution
+            {
+                if (a > x)
+                {
+                    a--;
+                }
+                else if (a < x) //
+                {
+                    a++;
+                }
+                if (b > y)
+                {
+                    b--;
+                }
+                else if (b < y) // 
+                {
+                    b++;
+                }
+                Console.WriteLine("a = " + a + " x = " + x);
+                Console.WriteLine("b = " + b + " y = " + y);
+                Console.WriteLine("--------------------------\n");
+            }
+            Console.WriteLine(" "+a+" "+b+" "+x+" "+y+" ");
+            Console.Read();
+
+        }
+    }
+}
